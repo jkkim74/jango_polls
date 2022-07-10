c42309162fb2aaa0ee2a1db6a8d0d13d8511f266
jkkim74 <jack68@naver.com>
2022-07-10 16:52:43 +0900

    poll function add

--- polls/admin.py
+++ polls/admin.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from .models import Question, Choice
 
 
-class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 
@@ -15,7 +15,12 @@ class QuestionAdmin(admin.ModelAdmin):
         ('Date information', {'fields': ['pub_date']}),
     ]
 
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+
     inlines = [ChoiceInline]
+
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
 
 
 admin.site.register(Question, QuestionAdmin)
